a0bcfd2a91c442aa4cec1ce830c5f0587ffcdd88
refixshow <adasscieszka@wp.pl>
2020-06-29 19:17:54 +0200

    y

--- src/Observer.js
+++ src/Observer.js
@@ -11,26 +11,20 @@ const Observer = ({
   const [elementClass, setElementClass] = useState("");
   const target = useRef(null);
 
-  let element = React.cloneElement(children, {
+  const element = React.cloneElement(children, {
     className: elementClass,
     ref: target,
   });
 
-  let callback = useCallback((entries) => {
+  const callback = useCallback((entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        setElementClass("show");
-      } else {
-        setElementClass("");
-      }
+      entry.isIntersecting ? setElementClass("show") : setElementClass("");
     });
   }, []);
 
-  let observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(callback, options);
 
   useEffect(() => {
-    // warning fix
-    // target.current podczas wywoływania cleanupu może się zmienić
     const targetToObserve = target.current;
 
     if (targetToObserve) {
